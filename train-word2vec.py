0ec233adecf4a87ac7c5110bc0c8138b5abcacf4
Hasanat Jahan <jahanhasanat@gmail.com>
2021-05-06 04:38:09 -0400

    model running with training data

--- train-word2vec.py
+++ train-word2vec.py
@@ -3,6 +3,8 @@ import gzip
 import gensim 
 import logging
 import bz2
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -33,3 +35,48 @@ def read_input(input_file):
 documents = list (read_input (input_file))
 logging.info ("Done reading data file")
 
+# training word2vec model 
+# model = gensim.models.Word2Vec (documents, vector_size=150, window=5, min_count=2, workers=10)
+
+# build vocabulary and train model
+model = gensim.models.Word2Vec(
+    documents,
+    vector_size=150,
+    window=5,
+    min_count=2,
+    workers=10,
+    epochs=10)
+
+model.train(documents,total_examples=len(documents),epochs=10)
+
+# Now to look at some output 
+# All together 
+
+#-----------------------------------
+# No. 1
+#-----------------------------------
+# similarity between two different words
+print("Similarity between 'dirty' and 'clean'")
+print(model.wv.similarity(w1="dirty",w2="clean"))
+
+print("Similarity between 'big' and 'dirty'")
+print(model.wv.similarity(w1="big",w2="dirty"))
+
+print("Similarity between 'big' and 'large'")
+print(model.wv.similarity(w1="big",w2="large"))
+
+print("Similarity between 'big' and 'small'")
+print(model.wv.similarity(w1="big",w2="small"))
+
+#----------------------------------
+# No. 2 
+#----------------------------------
+# look up top 6 words similar to 'polite'
+w1 = ["polite"]
+print("Top most similar words for polite")
+pp.pprint(model.wv.most_similar (positive=w1,topn=5))
+
+# look up top 6 words similar to 'orange'
+print("Top most similar words for orange")
+w1 = ["orange"]
+pp.pprint(model.wv.most_similar (positive=w1,topn=5))
